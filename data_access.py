d24324aa7aacd38d75717a694f686758c642c41a
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-06-19 17:40:48 +0200

    Add Create functionality

--- data_access.py
+++ data_access.py
@@ -14,3 +14,51 @@ def execute_sql(connection, sql):
         c.execute(sql)
     except Error as e:
         print(e)
+
+def create_database(db_file):
+    create_projects_table_sql = """
+    CREATE TABLE IF NOT EXISTS projects
+    (
+        id int NOT NULL,
+        name varchar(255) NOT NULL,
+        start_date text,
+        end_date text,
+        PRIMARY KEY(id)
+    );
+    """
+    create_tasks_table_sql = """
+    CREATE TABLE IF NOT EXISTS tasks
+    (
+        id int NOT NULL,
+        project_id int NOT NULL,
+        name varchar(255) NOT NULL,
+        description varchar(255),
+        status varchar(100) NOT NULL,
+        start_date text,
+        end_date text,
+        PRIMARY KEY(id),
+        FOREIGN KEY(project_id) REFERENCES projects(id)
+    );
+    """
+    conn = get_connection(db_file)
+    if conn:
+        execute_sql(conn, create_projects_table_sql)
+        execute_sql(conn, create_tasks_table_sql)
+    
+def add_project(conn, project):
+    sql = """
+    INSERT INTO projects (id, name, start_date, end_date) VALUES (?,?,?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    print("New project added")
+
+def add_task(conn, task):
+    sql = """INSERT INTO tasks (id, project_id, name, description, status, start_date, end_date) VALUES (?,?,?,?,?,?,?)"""
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+    print("New task added")
+
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import data_access
 
-
+# Add data to database
+project = (1, "Powtórka z angielskiego", "2020-05-11", "2021-06-10",)
+task = (1, 1, "Czasowniki regularne", "Zapamiętaj czasowniki ze strony 20", "Zakończone", "2020-05-11", "2020-05-12",)
 
 
 if __name__ == "__main__":
@@ -9,3 +11,6 @@ if __name__ == "__main__":
         exit(0)
     db_file = sys.argv[1]
     data_access.create_database(db_file)
+    connection = data_access.get_connection(db_file)
+    data_access.add_project(connection, project)
+    data_access.add_task(connection, task)
