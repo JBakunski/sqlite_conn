d2e1416c0b7b8c2ccd58997bf748482ff9874293
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-06-19 12:32:18 +0200

    Create database

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 import data_access
 
-sql = ""
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         exit(0)
     db_file = sys.argv[1]
-    conn = data_access.get_connection(db_file)
-    # data_access.execute_sql(conn, sql)
+    data_access.create_database(db_file)
